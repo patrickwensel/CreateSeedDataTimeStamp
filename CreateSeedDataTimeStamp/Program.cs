519603f146c62830cdc591aefbadeb1f487e5147
Patrick Wensel <pwensel@hotmail.com>
2021-12-31 11:27:43 +0300

    Add project files.

---
+++ CreateSeedDataTimeStamp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var currentTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+Console.WriteLine(currentTime);
+
